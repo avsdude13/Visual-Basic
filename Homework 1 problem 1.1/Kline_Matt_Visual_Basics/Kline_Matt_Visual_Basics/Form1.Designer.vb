bbebca670f5395fa1f2867dfb959ee985d0f1810
Matt Kline <mattkline13@gmail.com>
2018-02-11 23:30:44 -0700

    Homework 1.1

---
+++ Homework 1 problem 1.1/Kline_Matt_Visual_Basics/Kline_Matt_Visual_Basics/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HelloForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(HelloForm))
+        Me.MessageLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FrenchButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ItalianButton = New System.Windows.Forms.Button()
+        Me.PrintButton = New System.Windows.Forms.Button()
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.SuspendLayout()
+        '
+        'MessageLabel
+        '
+        Me.MessageLabel.Font = New System.Drawing.Font("Monotype Corsiva", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MessageLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.MessageLabel.Location = New System.Drawing.Point(40, 21)
+        Me.MessageLabel.Name = "MessageLabel"
+        Me.MessageLabel.Size = New System.Drawing.Size(625, 205)
+        Me.MessageLabel.TabIndex = 0
+        Me.MessageLabel.Text = "Click the below buttons to display Hello World a different language"
+        Me.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(77, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 1
+        '
+        'FrenchButton
+        '
+        Me.FrenchButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.FrenchButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FrenchButton.Location = New System.Drawing.Point(12, 224)
+        Me.FrenchButton.Name = "FrenchButton"
+        Me.FrenchButton.Size = New System.Drawing.Size(166, 48)
+        Me.FrenchButton.TabIndex = 2
+        Me.FrenchButton.Text = "French"
+        Me.FrenchButton.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(530, 224)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(166, 48)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = False
+        '
+        'ItalianButton
+        '
+        Me.ItalianButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ItalianButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ItalianButton.Location = New System.Drawing.Point(184, 224)
+        Me.ItalianButton.Name = "ItalianButton"
+        Me.ItalianButton.Size = New System.Drawing.Size(166, 48)
+        Me.ItalianButton.TabIndex = 5
+        Me.ItalianButton.Text = "Italian"
+        Me.ItalianButton.UseVisualStyleBackColor = False
+        '
+        'PrintButton
+        '
+        Me.PrintButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.PrintButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PrintButton.Location = New System.Drawing.Point(356, 224)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(166, 48)
+        Me.PrintButton.TabIndex = 6
+        Me.PrintButton.Text = "Print"
+        Me.PrintButton.UseVisualStyleBackColor = False
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'HelloForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Maroon
+        Me.ClientSize = New System.Drawing.Size(708, 284)
+        Me.Controls.Add(Me.PrintButton)
+        Me.Controls.Add(Me.ItalianButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FrenchButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.MessageLabel)
+        Me.Name = "HelloForm"
+        Me.Text = "Hello World by Matt Kline"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MessageLabel As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents FrenchButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents ItalianButton As System.Windows.Forms.Button
+    Friend WithEvents PrintButton As System.Windows.Forms.Button
+    Friend WithEvents PrintForm1 As Microsoft.VisualBasic.PowerPacks.Printing.PrintForm
+
+End Class
---
+++ Homework 1 problem 1.1/Kline_Matt_Visual_Basics/Kline_Matt_Visual_Basics/Form1.vb
@@ -0,0 +1,24 @@
+﻿' Program: Hello World Languages
+' Programmer: Matt Kline
+' Class: Intro to Visual Basics
+' Homework 1 Problem 1.1
+
+Public Class HelloForm
+
+    Private Sub FrenchButton_Click(sender As Object, e As EventArgs) Handles FrenchButton.Click
+        MessageLabel.Text = "Bonjour tout le monde" 'This displays Hello World after the button is pressed in French
+    End Sub
+
+    Private Sub ItalianButton_Click(sender As Object, e As EventArgs) Handles ItalianButton.Click
+        MessageLabel.Text = "Ciao Mondo" 'This displays Hello World after the button is pressed in Italian
+    End Sub
+
+    Private Sub PrintButton_Click(sender As Object, e As EventArgs) Handles PrintButton.Click
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close() 'This exits the program after the button is pressed
+    End Sub
+End Class
