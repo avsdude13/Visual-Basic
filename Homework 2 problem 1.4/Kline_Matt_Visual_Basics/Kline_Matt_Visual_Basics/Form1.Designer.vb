a80f95e0cce5ec5657eee5d58d5d27ebbd69035c
Matt Kline <mattkline13@gmail.com>
2018-02-11 23:33:56 -0700

    Homework 1.4

---
+++ Homework 2 problem 1.4/Kline_Matt_Visual_Basics/Kline_Matt_Visual_Basics/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CompanyInfo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CompanyInfo))
+        Me.MessageLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CompanyInfoButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.CallUsButton = New System.Windows.Forms.Button()
+        Me.DepartmentListButton = New System.Windows.Forms.Button()
+        Me.PrintButton = New System.Windows.Forms.Button()
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'MessageLabel
+        '
+        Me.MessageLabel.Font = New System.Drawing.Font("Monotype Corsiva", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MessageLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.MessageLabel.Location = New System.Drawing.Point(35, 4)
+        Me.MessageLabel.Name = "MessageLabel"
+        Me.MessageLabel.Size = New System.Drawing.Size(625, 128)
+        Me.MessageLabel.TabIndex = 0
+        Me.MessageLabel.Text = "Click the below buttons to be directed to a specific department"
+        Me.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(77, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 1
+        '
+        'CompanyInfoButton
+        '
+        Me.CompanyInfoButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.CompanyInfoButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CompanyInfoButton.Location = New System.Drawing.Point(45, 147)
+        Me.CompanyInfoButton.Name = "CompanyInfoButton"
+        Me.CompanyInfoButton.Size = New System.Drawing.Size(166, 48)
+        Me.CompanyInfoButton.TabIndex = 2
+        Me.CompanyInfoButton.Text = "Company Info"
+        Me.CompanyInfoButton.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(266, 212)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(166, 48)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = False
+        '
+        'CallUsButton
+        '
+        Me.CallUsButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.CallUsButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CallUsButton.Location = New System.Drawing.Point(266, 147)
+        Me.CallUsButton.Name = "CallUsButton"
+        Me.CallUsButton.Size = New System.Drawing.Size(166, 48)
+        Me.CallUsButton.TabIndex = 5
+        Me.CallUsButton.Text = "Call Us"
+        Me.CallUsButton.UseVisualStyleBackColor = False
+        '
+        'DepartmentListButton
+        '
+        Me.DepartmentListButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.DepartmentListButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DepartmentListButton.Location = New System.Drawing.Point(494, 147)
+        Me.DepartmentListButton.Name = "DepartmentListButton"
+        Me.DepartmentListButton.Size = New System.Drawing.Size(166, 48)
+        Me.DepartmentListButton.TabIndex = 6
+        Me.DepartmentListButton.Text = "Department List"
+        Me.DepartmentListButton.UseVisualStyleBackColor = False
+        '
+        'PrintButton
+        '
+        Me.PrintButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.PrintButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PrintButton.Location = New System.Drawing.Point(45, 212)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(166, 48)
+        Me.PrintButton.TabIndex = 8
+        Me.PrintButton.Text = "Print"
+        Me.PrintButton.UseVisualStyleBackColor = False
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Monotype Corsiva", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(513, 233)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(174, 27)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Programmer: Matt Kline"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CompanyInfo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Maroon
+        Me.ClientSize = New System.Drawing.Size(699, 272)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PrintButton)
+        Me.Controls.Add(Me.DepartmentListButton)
+        Me.Controls.Add(Me.CallUsButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.CompanyInfoButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.MessageLabel)
+        Me.Name = "CompanyInfo"
+        Me.Text = "My Fake Company by Matt Kline"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MessageLabel As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CompanyInfoButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents CallUsButton As System.Windows.Forms.Button
+    Friend WithEvents DepartmentListButton As System.Windows.Forms.Button
+    Friend WithEvents PrintButton As System.Windows.Forms.Button
+    Friend WithEvents PrintForm1 As Microsoft.VisualBasic.PowerPacks.Printing.PrintForm
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Homework 2 problem 1.4/Kline_Matt_Visual_Basics/Kline_Matt_Visual_Basics/Form1.vb
@@ -0,0 +1,27 @@
+﻿' Program: My Fake Company
+' Programmer: Matt Kline
+' Class: Intro to Visual Basics
+' Homework 1 Problem 1.3
+Public Class CompanyInfo
+
+    Private Sub CompanyInfoButton_Click(sender As Object, e As EventArgs) Handles CompanyInfoButton.Click
+        MessageLabel.Text = "My Fake Company 123 Main St Denver CO 80219" 'This displays the company info after the button is pressed
+    End Sub
+
+    Private Sub CallUsButton_Click(sender As Object, e As EventArgs) Handles CallUsButton.Click
+        MessageLabel.Text = "Reach us between 6am-10pm at 303-123-4567" 'This displays the phone number and contact time after the button is pressed
+    End Sub
+
+    Private Sub DepartmentListButton_Click(sender As Object, e As EventArgs) Handles DepartmentListButton.Click
+        MessageLabel.Text = "Sales ext 1 Receiving ext 2 Manager ext 3 Operator ext #" 'This displays department info after the button is pressed
+    End Sub
+
+    Private Sub PrintButton_Click(sender As Object, e As EventArgs) Handles PrintButton.Click
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close() 'This exits the program after the button is pressed
+    End Sub
+End Class
