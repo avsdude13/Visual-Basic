385aea6424aa0d5a09ce3309b29638ec6801ed07
Matt Kline <mattkline13@gmail.com>
2018-02-11 23:50:58 -0700

    Homework 8.1

---
+++ Homework 8.1/Homework 8.1/Homework 8.1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class IncomeSurveyForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(IncomeSurveyForm))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnterDataToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintAllInListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintAboveAverageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintPovertyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FamilySizeLabel = New System.Windows.Forms.Label()
+        Me.FamilySizeTextBox = New System.Windows.Forms.TextBox()
+        Me.IncomeSurveyLabel = New System.Windows.Forms.Label()
+        Me.IDTextBox = New System.Windows.Forms.TextBox()
+        Me.IDLabel = New System.Windows.Forms.Label()
+        Me.IncomeTextBox = New System.Windows.Forms.TextBox()
+        Me.IncomeLabel = New System.Windows.Forms.Label()
+        Me.PrintAll = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.PrintBelowPoverty = New System.Drawing.Printing.PrintDocument()
+        Me.PrintAboveAverage = New System.Drawing.Printing.PrintDocument()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ReportsToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(390, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EnterDataToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'EnterDataToolStripMenuItem
+        '
+        Me.EnterDataToolStripMenuItem.Name = "EnterDataToolStripMenuItem"
+        Me.EnterDataToolStripMenuItem.Size = New System.Drawing.Size(128, 22)
+        Me.EnterDataToolStripMenuItem.Text = "&Enter Data"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(125, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(128, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'ReportsToolStripMenuItem
+        '
+        Me.ReportsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintAllInListToolStripMenuItem, Me.PrintPovertyToolStripMenuItem, Me.PrintAboveAverageToolStripMenuItem})
+        Me.ReportsToolStripMenuItem.Name = "ReportsToolStripMenuItem"
+        Me.ReportsToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.ReportsToolStripMenuItem.Text = "&Reports"
+        '
+        'PrintAllInListToolStripMenuItem
+        '
+        Me.PrintAllInListToolStripMenuItem.Name = "PrintAllInListToolStripMenuItem"
+        Me.PrintAllInListToolStripMenuItem.Size = New System.Drawing.Size(182, 22)
+        Me.PrintAllInListToolStripMenuItem.Text = "&Print All in List"
+        '
+        'PrintAboveAverageToolStripMenuItem
+        '
+        Me.PrintAboveAverageToolStripMenuItem.Name = "PrintAboveAverageToolStripMenuItem"
+        Me.PrintAboveAverageToolStripMenuItem.Size = New System.Drawing.Size(182, 22)
+        Me.PrintAboveAverageToolStripMenuItem.Text = "Print Above &Average"
+        '
+        'PrintPovertyToolStripMenuItem
+        '
+        Me.PrintPovertyToolStripMenuItem.Name = "PrintPovertyToolStripMenuItem"
+        Me.PrintPovertyToolStripMenuItem.Size = New System.Drawing.Size(182, 22)
+        Me.PrintPovertyToolStripMenuItem.Text = "Print &Below Poverty"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'FamilySizeLabel
+        '
+        Me.FamilySizeLabel.AutoSize = True
+        Me.FamilySizeLabel.Location = New System.Drawing.Point(21, 125)
+        Me.FamilySizeLabel.Name = "FamilySizeLabel"
+        Me.FamilySizeLabel.Size = New System.Drawing.Size(59, 13)
+        Me.FamilySizeLabel.TabIndex = 4
+        Me.FamilySizeLabel.Text = "Family Size"
+        '
+        'FamilySizeTextBox
+        '
+        Me.FamilySizeTextBox.Location = New System.Drawing.Point(43, 150)
+        Me.FamilySizeTextBox.Name = "FamilySizeTextBox"
+        Me.FamilySizeTextBox.Size = New System.Drawing.Size(309, 20)
+        Me.FamilySizeTextBox.TabIndex = 5
+        '
+        'IncomeSurveyLabel
+        '
+        Me.IncomeSurveyLabel.AutoSize = True
+        Me.IncomeSurveyLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.IncomeSurveyLabel.Location = New System.Drawing.Point(120, 34)
+        Me.IncomeSurveyLabel.Name = "IncomeSurveyLabel"
+        Me.IncomeSurveyLabel.Size = New System.Drawing.Size(154, 25)
+        Me.IncomeSurveyLabel.TabIndex = 1
+        Me.IncomeSurveyLabel.Text = "Income Survey"
+        '
+        'IDTextBox
+        '
+        Me.IDTextBox.Location = New System.Drawing.Point(43, 91)
+        Me.IDTextBox.Name = "IDTextBox"
+        Me.IDTextBox.Size = New System.Drawing.Size(309, 20)
+        Me.IDTextBox.TabIndex = 3
+        '
+        'IDLabel
+        '
+        Me.IDLabel.AutoSize = True
+        Me.IDLabel.Location = New System.Drawing.Point(21, 66)
+        Me.IDLabel.Name = "IDLabel"
+        Me.IDLabel.Size = New System.Drawing.Size(95, 13)
+        Me.IDLabel.TabIndex = 2
+        Me.IDLabel.Text = "Identification Code"
+        '
+        'IncomeTextBox
+        '
+        Me.IncomeTextBox.Location = New System.Drawing.Point(43, 208)
+        Me.IncomeTextBox.Name = "IncomeTextBox"
+        Me.IncomeTextBox.Size = New System.Drawing.Size(309, 20)
+        Me.IncomeTextBox.TabIndex = 7
+        '
+        'IncomeLabel
+        '
+        Me.IncomeLabel.AutoSize = True
+        Me.IncomeLabel.Location = New System.Drawing.Point(21, 183)
+        Me.IncomeLabel.Name = "IncomeLabel"
+        Me.IncomeLabel.Size = New System.Drawing.Size(42, 13)
+        Me.IncomeLabel.TabIndex = 6
+        Me.IncomeLabel.Text = "Income"
+        '
+        'PrintAll
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'PrintBelowPoverty
+        '
+        '
+        'PrintAboveAverage
+        '
+        '
+        'IncomeSurveyForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(390, 246)
+        Me.Controls.Add(Me.IncomeTextBox)
+        Me.Controls.Add(Me.IncomeLabel)
+        Me.Controls.Add(Me.IDTextBox)
+        Me.Controls.Add(Me.IDLabel)
+        Me.Controls.Add(Me.IncomeSurveyLabel)
+        Me.Controls.Add(Me.FamilySizeTextBox)
+        Me.Controls.Add(Me.FamilySizeLabel)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "IncomeSurveyForm"
+        Me.Text = "Income Survey"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EnterDataToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ReportsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FamilySizeLabel As System.Windows.Forms.Label
+    Friend WithEvents FamilySizeTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents IncomeSurveyLabel As System.Windows.Forms.Label
+    Friend WithEvents IDTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents IDLabel As System.Windows.Forms.Label
+    Friend WithEvents IncomeTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents IncomeLabel As System.Windows.Forms.Label
+    Friend WithEvents PrintAll As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As System.Windows.Forms.PrintPreviewDialog
+    Friend WithEvents PrintBelowPoverty As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintAboveAverage As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintAboveAverageToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintPovertyToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintAllInListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Homework 8.1/Homework 8.1/Homework 8.1.vb
@@ -0,0 +1,206 @@
+﻿' Program: Homework 8.1
+' Programmer: Matt Kline
+' Date 4/26
+
+Public Class IncomeSurveyForm
+
+    Private Income(10) As IncomeSurvey
+    Private IDNumber, FamilyMemberCount, ColumnInteger As Integer
+    Private AnnualIncome As Decimal
+    Private validentry As Boolean
+    Private TotalCount As Integer = 0
+    Private BelowPovertyCount As Integer = 0
+    Private AboveAverageCount As Integer = 0
+    Const POVERTYLINE As Integer = 12000
+
+    Structure IncomeSurvey
+        Dim IDNumberInteger As Integer
+        Dim FamilyMemberInteger As Integer
+        Dim AnnualIncomeDecimal As Decimal
+    End Structure
+
+    Function Clear() As Boolean
+        Dim done As Boolean
+        IDTextBox.Clear()
+        FamilySizeTextBox.Clear()
+        IncomeTextBox.Clear()
+        With IDTextBox
+            .Focus()
+        End With
+        done = True
+        Return done
+    End Function
+
+    Private Sub EnterDataToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EnterDataToolStripMenuItem.Click
+        validentry = True
+        Try
+            IDNumber = Integer.Parse(IDTextBox.Text)
+            Try
+                FamilyMemberCount = Integer.Parse(FamilySizeTextBox.Text)
+                Try
+                    AnnualIncome = Integer.Parse(IncomeTextBox.Text)
+                Catch NumberFormat As Exception
+                    MessageBox.Show("Enter Income as an integer.", "Selection Incomplete", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    With IncomeTextBox
+                        .Focus()
+                        .SelectAll()
+                        validentry = False
+                    End With
+                End Try
+            Catch NumberFormat As Exception
+                MessageBox.Show("Enter Family Members as an integer.", "Selection Incomplete", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                With FamilySizeTextBox
+                    .Focus()
+                    .SelectAll()
+                    validentry = False
+                End With
+            End Try
+        Catch NumberFormat As Exception
+            MessageBox.Show("Enter ID Number as an integer.", "Selection Incomplete", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            With IDTextBox
+                .Focus()
+                .SelectAll()
+                validentry = False
+            End With
+        End Try
+
+        If validentry Then
+            Income(TotalCount).IDNumberInteger = IDNumber
+            Income(TotalCount).FamilyMemberInteger = FamilyMemberCount
+            Income(TotalCount).AnnualIncomeDecimal = AnnualIncome
+            TotalCount += 1
+            If AnnualIncome <= POVERTYLINE Then
+                BelowPovertyCount += 1
+            Else
+                AboveAverageCount += 1
+            End If
+            Clear()
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Dim ResponseDialogResult As DialogResult
+
+        ResponseDialogResult = MessageBox.Show("Print the report?", "Terminate the Application", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If ResponseDialogResult = Windows.Forms.DialogResult.Yes Then
+            PrintAllInListToolStripMenuItem_Click(sender, e)
+        End If
+        Me.Close()
+    End Sub
+
+    Private Sub PrintAllInListToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintAllInListToolStripMenuItem.Click
+        PrintPreviewDialog1.Document = PrintAll
+        PrintPreviewDialog1.ShowDialog()
+    End Sub
+
+    Private Sub PrintPovertyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintPovertyToolStripMenuItem.Click
+        PrintPreviewDialog1.Document = PrintBelowPoverty
+        PrintPreviewDialog1.ShowDialog()
+    End Sub
+
+    Private Sub PrintAboveAverageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintAboveAverageToolStripMenuItem.Click
+        PrintPreviewDialog1.Document = PrintAboveAverage
+        PrintPreviewDialog1.ShowDialog()
+    End Sub
+
+    Private Sub PrintAll_PrintPage(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintAll.PrintPage
+        Dim PrintFont As New Font("Arial", 12)
+        Dim HeadingFont As New Font("Arial", 14, FontStyle.Bold)
+        Dim LineHeightSingle As Single = PrintFont.GetHeight + 2
+        Dim Column1HorizontalLocationSingle As Single = 300
+        Dim Column2Horizonta1Locationsingle As Single = 365
+        Dim Column3Horizonta1Locationsingle As Single = e.MarginBounds.Left
+        Dim Column4Horizonta1Locationsingle As Single = 150
+        Dim Column5Horizonta1Locationsingle As Single = 300
+        Dim Column6Horizonta1Locationsingle As Single = 450
+
+        Dim VerticalPrintLocationSingle As Single = e.MarginBounds.Top
+        Dim FontSizeF As New SizeF
+
+        e.Graphics.DrawString("Poverty Guidelines for 2008", HeadingFont, Brushes.Black, Column1HorizontalLocationSingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle
+        e.Graphics.DrawString("By Matt Kline", PrintFont, Brushes.Black, Column2Horizonta1Locationsingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle * 2
+
+        e.Graphics.DrawString("There are " + TotalCount.ToString + " Surveys in the list. The list is as follows", PrintFont, Brushes.Black, Column3Horizonta1Locationsingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle
+
+        For Each IncomeSurveyData As IncomeSurvey In Income
+            If IncomeSurveyData.IDNumberInteger > 0 Then
+                e.Graphics.DrawString("ID Number: " + IncomeSurveyData.IDNumberInteger.ToString, PrintFont, Brushes.Black, Column4Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                e.Graphics.DrawString("Family Size: " + IncomeSurveyData.FamilyMemberInteger.ToString, PrintFont, Brushes.Black, Column5Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                e.Graphics.DrawString("Income: $" + FormatNumber(IncomeSurveyData.AnnualIncomeDecimal), PrintFont, Brushes.Black, Column6Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                VerticalPrintLocationSingle += LineHeightSingle
+            End If
+        Next
+    End Sub
+
+    Private Sub PrintBelowPoverty_PrintPage(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintBelowPoverty.PrintPage
+        Dim PrintFont As New Font("Arial", 12)
+        Dim HeadingFont As New Font("Arial", 14, FontStyle.Bold)
+        Dim LineHeightSingle As Single = PrintFont.GetHeight + 2
+        Dim Column1HorizontalLocationSingle As Single = 300
+        Dim Column2Horizonta1Locationsingle As Single = 365
+        Dim Column3Horizonta1Locationsingle As Single = e.MarginBounds.Left
+        Dim Column4Horizonta1Locationsingle As Single = 150
+        Dim Column5Horizonta1Locationsingle As Single = 300
+        Dim Column6Horizonta1Locationsingle As Single = 450
+
+        Dim VerticalPrintLocationSingle As Single = e.MarginBounds.Top
+        Dim FontSizeF As New SizeF
+
+        e.Graphics.DrawString("Poverty Guidelines for 2008", HeadingFont, Brushes.Black, Column1HorizontalLocationSingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle
+        e.Graphics.DrawString("By Matt Kline", PrintFont, Brushes.Black, Column2Horizonta1Locationsingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle * 2
+
+        e.Graphics.DrawString("There are " + TotalCount.ToString + " Surveys in the list. Of those " + BelowPovertyCount.ToString + " are below the poverty line", PrintFont, Brushes.Black, Column3Horizonta1Locationsingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle
+
+        For Each IncomeSurveyData As IncomeSurvey In Income
+            If IncomeSurveyData.IDNumberInteger > 0 And IncomeSurveyData.AnnualIncomeDecimal <= POVERTYLINE Then
+                e.Graphics.DrawString("ID Number: " + IncomeSurveyData.IDNumberInteger.ToString, PrintFont, Brushes.Black, Column4Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                e.Graphics.DrawString("Family Size: " + IncomeSurveyData.FamilyMemberInteger.ToString, PrintFont, Brushes.Black, Column5Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                e.Graphics.DrawString("Income: $" + FormatNumber(IncomeSurveyData.AnnualIncomeDecimal), PrintFont, Brushes.Black, Column6Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                VerticalPrintLocationSingle += LineHeightSingle
+            End If
+        Next
+    End Sub
+
+    Private Sub PrintAboveAverage_PrintPage(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintAboveAverage.PrintPage
+        Dim PrintFont As New Font("Arial", 12)
+        Dim HeadingFont As New Font("Arial", 14, FontStyle.Bold)
+        Dim LineHeightSingle As Single = PrintFont.GetHeight + 2
+        Dim Column1HorizontalLocationSingle As Single = 300
+        Dim Column2Horizonta1Locationsingle As Single = 365
+        Dim Column3Horizonta1Locationsingle As Single = e.MarginBounds.Left
+        Dim Column4Horizonta1Locationsingle As Single = 150
+        Dim Column5Horizonta1Locationsingle As Single = 300
+        Dim Column6Horizonta1Locationsingle As Single = 450
+
+        Dim VerticalPrintLocationSingle As Single = e.MarginBounds.Top
+        Dim FontSizeF As New SizeF
+
+        e.Graphics.DrawString("Poverty Guidelines for 2008", HeadingFont, Brushes.Black, Column1HorizontalLocationSingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle
+        e.Graphics.DrawString("By Matt Kline", PrintFont, Brushes.Black, Column2Horizonta1Locationsingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle * 2
+
+        e.Graphics.DrawString("There are " + TotalCount.ToString + " Surveys in the list. Of those " + AboveAverageCount.ToString + " are above average income.", PrintFont, Brushes.Black, Column3Horizonta1Locationsingle, VerticalPrintLocationSingle)
+        VerticalPrintLocationSingle += LineHeightSingle
+
+        For Each IncomeSurveyData As IncomeSurvey In Income
+            If IncomeSurveyData.IDNumberInteger > 0 And IncomeSurveyData.AnnualIncomeDecimal > POVERTYLINE Then
+                e.Graphics.DrawString("ID Number: " + IncomeSurveyData.IDNumberInteger.ToString, PrintFont, Brushes.Black, Column4Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                e.Graphics.DrawString("Family Size: " + IncomeSurveyData.FamilyMemberInteger.ToString, PrintFont, Brushes.Black, Column5Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                e.Graphics.DrawString("Income: $" + FormatNumber(IncomeSurveyData.AnnualIncomeDecimal), PrintFont, Brushes.Black, Column6Horizonta1Locationsingle, VerticalPrintLocationSingle)
+                VerticalPrintLocationSingle += LineHeightSingle
+            End If
+        Next
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutBox1.ShowDialog()
+    End Sub
+End Class
